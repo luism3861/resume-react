1609bbb0ee9e7952b191ea7d38c1a62331eba7dc
luism3861 <Eduardo_Medina@fragua.com.mx>
2022-01-19 11:54:34 -0600

    remove dependencies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './css/App.css';
 import Hero from './components/Hero';
 
 const App = ({ resumeJson }) => {
   return (
-    <Router>
       <div className='app'>
         <Hero resumeObj={resumeJson} />
       </div>
-    </Router>
   );
 };
 
